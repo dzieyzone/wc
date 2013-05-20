21a7764d5c2d7c6f13b24becd419093c56c4948b
Jay <dzieyzone@gmail.com>
2013-05-20 13:58:14 +0800

    update

--- themes/fusion_widget/views-view-field--blogs--field-img-src-value.tpl.php
+++ themes/fusion_widget/views-view-field--blogs--field-img-src-value.tpl.php
@@ -1,8 +1,8 @@
 <?php
   $image_url = '';
   $preset = 'blog_teaser';
-  //print $output;
   if (!empty($output)){
+  	print $output;
     $image_url = imagecache_external_generate_path($output, $preset);
     print "<img src=\"$image_url\" />";
   }
