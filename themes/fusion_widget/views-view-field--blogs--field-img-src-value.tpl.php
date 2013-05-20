81ed27970a8de788fe73474481a1ab8310dd2a7f
Jay <dzieyzone@gmail.com>
2013-05-20 14:18:34 +0800

    find http

--- themes/fusion_widget/views-view-field--blogs--field-img-src-value.tpl.php
+++ themes/fusion_widget/views-view-field--blogs--field-img-src-value.tpl.php
@@ -1,8 +1,8 @@
 <?php
   $image_url = '';
   $preset = 'blog_teaser';
-  if (!empty($output)){
-  	print $output;
+  $pos = strpos($output, 'http');
+  if ($pos !== false){
     $image_url = imagecache_external_generate_path($output, $preset);
     print "<img src=\"$image_url\" />";
   }
