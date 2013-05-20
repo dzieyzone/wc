d90ab3b83e2cec8935b9be0dbfa08a3d81b7bbc7
Jay <dzieyzone@gmail.com>
2013-05-20 13:50:15 +0800

    update

--- themes/fusion_widget/views-view-field--blogs--field-img-src-value.tpl.php
+++ themes/fusion_widget/views-view-field--blogs--field-img-src-value.tpl.php
@@ -1,7 +1,8 @@
 <?php
   $image_url = '';
   $preset = 'blog_teaser';
-  if ($url = $output){
+  //print $output;
+  if (!empty($output)){
     $image_url = imagecache_external_generate_path($url, $preset);
     print "<img src=\"$image_url\" />";
   }
