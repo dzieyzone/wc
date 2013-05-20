eacd285b2a6b081deecaebd6f71ffc3b6ea6773c
Jay <dzieyzone@gmail.com>
2013-05-20 13:56:02 +0800

    stage

--- themes/fusion_widget/views-view-field--blogs--field-img-src-value.tpl.php
+++ themes/fusion_widget/views-view-field--blogs--field-img-src-value.tpl.php
@@ -3,7 +3,7 @@
   $preset = 'blog_teaser';
   //print $output;
   if (!empty($output)){
-    $image_url = imagecache_external_generate_path($url, $preset);
+    $image_url = imagecache_external_generate_path($output, $preset);
     print "<img src=\"$image_url\" />";
   }
   else {
